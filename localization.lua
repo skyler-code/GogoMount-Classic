2c36f4bfc3441413a93ec2f3f7b7dfca4140fe9e
Skyler H <skylerh15@github.com>
2022-08-21 19:08:27 -0500

    initial addon

---
+++ localization.lua
@@ -0,0 +1,49 @@
+﻿
+---------
+--function GoGo_Localize()
+---------
+
+-- Constants
+	GoGo_Variables.Localize.ColdWeatherFlying = 54197
+	GoGo_Variables.Localize.FastFlightForm = 40120
+	GoGo_Variables.Localize.FlightForm = 33943
+	GoGo_Variables.Localize.AquaForm = 1066
+	GoGo_Variables.Localize.TravelForm = 783
+	GoGo_Variables.Localize.CatForm = 768
+	GoGo_Variables.Localize.GhostWolf = 2645
+	GoGo_Variables.Localize.CrusaderAura = 32223
+	GoGo_Variables.Localize.AspectCheetah = 5118
+--	GoGo_Variables.Localize.AspectPack = 13159
+
+-- Default to english, localized below if translation is available (see http://wow.curseforge.com/projects/gogomount/localization/)
+	GoGo_Variables.Localize.Zone.TwistingNether = "Twisting Nether"
+	GoGo_Variables.Localize.Zone.TheFrozenSea = "The Frozen Sea"
+	GoGo_Variables.Localize.Zone.AQ40 = "Ahn'Qiraj"
+	GoGo_Variables.Localize.Zone.Dalaran = "Dalaran"
+	GOGO_SZONE_THEUNDERBELLY = "The Underbelly"
+	GOGO_SZONE_KRASUSLANDING = "Krasus' Landing"
+	GOGO_SZONE_THEVIOLETCITADEL = "The Violet Citadel"
+	GoGo_Variables.Localize.Zone.Wintergrasp = "Wintergrasp"
+	GoGo_Variables.Localize.Zone.SholazarBasin = "Sholazar Basin"
+	GoGo_Variables.Localize.Zone.TheStormPeaks = "The Storm Peaks"
+	GOGO_ZONE_ICECROWN = "Icecrown"
+	GOGO_ZONE_THEOCULUS = "The Oculus"
+	GOGO_SKILL_ENGINEERING = "Engineering"
+	GOGO_SKILL_TAILORING = "Tailoring"
+	GOGO_SKILL_RIDING = "Riding"
+	GOGO_STRING_ENABLEAUTODISMOUNT = "Enable automatic dismount"
+	GOGO_STRING_DRUIDSINGLECLICK = "Single click to shift from form to travel forms"
+	GoGo_Variables.Localize.String.DruidFlightPreference = "Always use flight forms instead of when moving only"
+	GOGO_STRING_SAMEEPICFLYSPEED = "Consider 310% and 280% mounts the same speed"
+	GOGO_STRING_DISABLEUPDATENOTICES = "Disable update notices"
+	GOGO_STRING_DISABLEUNKNOWNMOUNTNOTICES = "Disable unknown mount notices"
+	GOGO_STRING_UNKNOWNMOUNTFOUND = "An unknown mount has been found in you list of mounts.  Please report this so that it can be added to future versions of GoGoMount."
+	GOGO_STRING_NEWVERSIONFOUND = "GoGoMount update is available and is being used by "
+	GOGO_STRING_FAVORITES = "Favorites"
+	GOGO_STRING_CURRENTZONEFAVORITES = "Current Zone Favorites"
+	GOGO_STRING_GLOBALFAVORITES = "Global Favorites"
+	GOGO_STRING_GLOBALPREFERENCES = "Global Preferences"
+	GOGO_TALENT_FERALSWIFTNESS = "Feral Swiftness"
+	GOGO_TALENT_IMPROVEDGHOSTWOLF = "Improved Ghost Wolf"
+
+--end --function
